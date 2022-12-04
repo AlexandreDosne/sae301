fa4ae67a75983c867d5c0ca1163d36bc0b05eae4
Alexandre Dosne <alexandre@dosne.com>
2022-12-04 15:37:33 +0100

    MaJ Panier

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -7,8 +7,25 @@ if (Cookies.get('cart') != undefined)
     AfficherPanier();
 }
 
+document.addEventListener('click', function (event)
+{
+    if (!event.target.matches('.jsCartPlus')) return;
+    event.preventDefault();
+    ModifierQty(event.target.getAttribute('targ-id'));
+});
+
+document.addEventListener('click', function (event)
+{
+    if (!event.target.matches('.jsCartMinus')) return;
+    event.preventDefault();
+    ModifierQty(event.target.getAttribute('targ-id'), false);
+});
+
 function AfficherPanier()
 {
+    elCartList.innerHTML = '';
+    elPrixTotal.innerText = '';
+
     var panier = JSON.parse(Cookies.get('cart'));
     var prixTotal = 0;
 
@@ -17,10 +34,47 @@ function AfficherPanier()
         let ss = (el.quantite > 1) ? 's' : '';
         let prixFinal = el.tarif * el.quantite;
 
-        elCartList.innerHTML += `<li>${el.titre} - ${el.quantite} ticket${ss} (${prixFinal}€)</li>`;
+        elCartList.innerHTML += `<li>${el.titre} - ${el.quantite} ticket${ss} (${prixFinal}€) <button targ-id="${el.id}" class="jsCartPlus">+<button><button targ-id="${el.id}" class="jsCartMinus">-<button></li>`;
 
         prixTotal += prixFinal;
     });
 
-    elPrixTotal.innerText += prixTotal.toString() + '€';
+    if (prixTotal == 0)
+        elPrixTotal.innerText += 'Votre panier est vide.';
+    else
+        elPrixTotal.innerText += prixTotal.toString() + '€';
+}
+
+// Incremente la quantite d'un article si present
+function ModifierQty(cid, plus = true)
+{
+    let cookies = JSON.parse(Cookies.get('cart'));
+
+    cookies.forEach(el =>
+    {
+        if (el.id == cid)
+        {
+            if (plus)
+            {
+                el.quantite++;
+            }
+            else
+            {
+                if (el.quantite > 0)
+                {
+                    el.quantite--;
+
+                    if (el.quantite == 0)
+                    {
+                        let thisIndex = cookies.indexOf(el);
+                        cookies.splice(thisIndex, 1);
+                    }
+                }
+            }
+
+            Cookies.set('cart', JSON.stringify(cookies), { path: '/' });
+            AfficherPanier();
+            return;
+        }
+    });
 }
