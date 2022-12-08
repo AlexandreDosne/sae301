fb44309f073984f7b47e3d95e826abcbc9d2e445
BaptisteCuny <baptiste.cuny@etudiant.univ-reims.fr>
2022-12-08 20:22:23 +0100

    maj filtter css

--- public/assets/js/listFilter.js
+++ public/assets/js/listFilter.js
@@ -54,22 +54,34 @@ document.getElementById('jsCat0').addEventListener('click', () =>
     FilterItems(categories.THEATER);
     filterbutton1.classList.toggle('filter-active');
     filterbutton.classList.remove('filter-active');
+    filterbutton2.classList.remove('filter-active');
+    filterbutton3.classList.remove('filter-active');
+    filterbutton4.classList.remove('filter-active');
 });
 document.getElementById('jsCat1').addEventListener('click', () =>
 {
     FilterItems(categories.CONCERT);
     filterbutton2.classList.toggle('filter-active');
     filterbutton.classList.remove('filter-active');
+    filterbutton1.classList.remove('filter-active');
+    filterbutton3.classList.remove('filter-active');
+    filterbutton4.classList.remove('filter-active');
 });
 document.getElementById('jsCat2').addEventListener('click', () =>
 {
     FilterItems(categories.SPECTACLE);
     filterbutton3.classList.toggle('filter-active');
     filterbutton.classList.remove('filter-active');
+    filterbutton1.classList.remove('filter-active');
+    filterbutton2.classList.remove('filter-active');
+    filterbutton4.classList.remove('filter-active');
 });
 document.getElementById('jsCat3').addEventListener('click', () =>
 {
     FilterItems(categories.EXPOSITION);
     filterbutton4.classList.toggle('filter-active');
     filterbutton.classList.remove('filter-active');
+    filterbutton1.classList.remove('filter-active');
+    filterbutton2.classList.remove('filter-active');
+    filterbutton3.classList.remove('filter-active');
 });
