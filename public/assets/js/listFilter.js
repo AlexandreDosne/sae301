8b105189a19501bcc21597dc3eae901906728a48
Alexandre Dosne <alexandre@dosne.com>
2022-12-08 19:06:37 +0100

    Fix et optimisation

--- public/assets/js/listFilter.js
+++ public/assets/js/listFilter.js
@@ -14,6 +14,12 @@ const listingItems = [
     document.getElementsByClassName('catExposition')
 ];
 
+const filterbutton = document.getElementById('jsCatAll');
+const filterbutton1 = document.getElementById('jsCat0');
+const filterbutton2 = document.getElementById('jsCat1');
+const filterbutton3 = document.getElementById('jsCat2');
+const filterbutton4 = document.getElementById('jsCat3');
+
 function FilterItems(category)
 {
     for (let i = 0; i < listingItems.length; ++i)
@@ -34,10 +40,36 @@ function ShowAllItems()
             listingItems[i][j].hidden = false;
         }
     }
+
+    filterbutton.classList.toggle('filter-active');
+    filterbutton1.classList.remove('filter-active');
+    filterbutton2.classList.remove('filter-active');
+    filterbutton3.classList.remove('filter-active');
+    filterbutton4.classList.remove('filter-active');
 }
 
 document.getElementById('jsCatAll').addEventListener('click', ShowAllItems);
-document.getElementById('jsCat0').addEventListener('click', () => { FilterItems(categories.THEATER); });
-document.getElementById('jsCat1').addEventListener('click', () => { FilterItems(categories.CONCERT); });
-document.getElementById('jsCat2').addEventListener('click', () => { FilterItems(categories.SPECTACLE); });
-document.getElementById('jsCat3').addEventListener('click', () => { FilterItems(categories.EXPOSITION); });
+document.getElementById('jsCat0').addEventListener('click', () =>
+{
+    FilterItems(categories.THEATER);
+    filterbutton1.classList.toggle('filter-active');
+    filterbutton.classList.remove('filter-active');
+});
+document.getElementById('jsCat1').addEventListener('click', () =>
+{
+    FilterItems(categories.CONCERT);
+    filterbutton2.classList.toggle('filter-active');
+    filterbutton.classList.remove('filter-active');
+});
+document.getElementById('jsCat2').addEventListener('click', () =>
+{
+    FilterItems(categories.SPECTACLE);
+    filterbutton3.classList.toggle('filter-active');
+    filterbutton.classList.remove('filter-active');
+});
+document.getElementById('jsCat3').addEventListener('click', () =>
+{
+    FilterItems(categories.EXPOSITION);
+    filterbutton4.classList.toggle('filter-active');
+    filterbutton.classList.remove('filter-active');
+});
