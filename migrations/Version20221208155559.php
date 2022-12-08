5dc20355ac41a037f2428eb11466893516a552ca
BaptisteCuny <baptiste.cuny@etudiant.univ-reims.fr>
2022-12-08 17:25:21 +0100

    css profil

--- migrations/Version20221208155559.php
+++ migrations/Version20221208155559.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221208130658 extends AbstractMigration
+final class Version20221208155559 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20221208130658 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE achat (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, date DATETIME NOT NULL, montant INT NOT NULL, content VARCHAR(255) NOT NULL, INDEX IDX_26A98456A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE achat ADD CONSTRAINT FK_26A98456A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE manifestation CHANGE description description VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE achat DROP FOREIGN KEY FK_26A98456A76ED395');
+        $this->addSql('DROP TABLE achat');
         $this->addSql('ALTER TABLE manifestation CHANGE description description VARCHAR(600) NOT NULL');
     }
 }
